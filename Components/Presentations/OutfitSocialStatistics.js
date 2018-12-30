b9a75efd71e121e114ce0b648e7885b8795737ec
Deven Bryant <chromosome24@gmail.com>
2018-12-30 00:08:37 -0800

    modified OutfitSocialStatistics

--- Components/Presentations/OutfitSocialStatistics.js
+++ Components/Presentations/OutfitSocialStatistics.js
@@ -10,7 +10,7 @@ import {SvgIcon} from '../SvgAssets/SvgIcon.js';
 )*/
 
 export const OutfitSocialStatistics = (props) => (
-	<div className={OutfitStyles.SocialStatsContainer} >
+	<div className={OutfitStyles.SocialStatsContainerHome} >
 		<div className={OutfitStyles.socialStatsIconContainer} >
 				<SvgIcon className={OutfitStyles.socialStatsIcon} name="HeartIcon" fill="none" stroke="#AAA" />
 		</div>
